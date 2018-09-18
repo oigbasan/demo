0906e5c2ec7254f2996aeb6cfb4511cf55ca1b86
oigbasan <oigbasan@uwo.ca>
2018-09-18 11:29:01 -0400

    more changed to file

--- First_try.py
+++ First_try.py
@@ -14,3 +14,14 @@ c=-510
 x1=(-b+math.sqrt(b**2 - 4*a*c))/(2*a)
 print(x1)
 print("This is my first exercise")
+mynum=0
+count=0
+for i in range(5):
+    mynum+=1
+    count+=1
+    if mynum==3:
+        print("here you go "+ str(mynum))
+        break
+    
+print("out of loop after " +str(count) + " counts")
+        
