9a5a7f094012685050eaea7e2bd20e69ac1495f7
oigbasan <oigbasan@uwo.ca>
2018-09-18 11:13:40 -0400

    initial commit

---
+++ First_try.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 16 17:25:12 2018
+
+@author: segun
+
+"""
+
+"""34*x^2+ 64*x -510=0 """
+import math
+a=34
+b=68
+c=-510
+x1=(-b+math.sqrt(b**2 - 4*a*c))/(2*a)
+print(x1)
+print("This is my first exercise")
